cc633c68992008b6dc36c987465c0e22f7b6b1a7
Marcin <marcinu456@gmail.com>
2021-06-10 00:10:30 +0200

    change python

--- Lista5/Chaos/plotEpidemy.py
+++ Lista5/Chaos/plotEpidemy.py
@@ -8,15 +8,15 @@ import matplotlib.pyplot as plt
 """
 
 
-s, i, r = np.loadtxt(sys.argv[1], unpack=True)
+s, i, r, q,w,e = np.loadtxt(sys.argv[1], unpack=True)
 
-plt.plot(range(len(s)), s, "b-", label="podatny")
-plt.plot(range(len(s)), i, "r-", label="zainfekowany")
-plt.plot(range(len(s)), r, "g-", label="ozdrowiony")
+plt.plot(range(len(s)), s, "b-", label="ek")
+plt.plot(range(len(s)), i, "r-", label="ep")
+plt.plot(range(len(s)), r, "g-", label="ec")
 #plt.plot(t, t**(1.75), "b-", label="N**(1.74)")
-plt.title("population " + sys.argv[1])
+
 plt.xlabel("czas")
-plt.ylabel("liczba")
+plt.ylabel("energia")
 
 
 
